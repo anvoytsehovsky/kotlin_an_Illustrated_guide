1eb48e82e19f31a009fb96ff1d4a2a464ac1de2e
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 15:17:33 +0300

    Listing 2.20 - Accepting the default argument for the first parameter of a function.

--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,9 @@ fun main() {
 
     fun speed(distance: Double = 42.195, time: Double) = distance / time
 
-    val walkingSpeed = speed(8.27)
-    val bikingSpeed = speed(2.85)
-    val drivingSpeed = speed(0.37)
+    val walkingSpeed = speed(time = 8.27)
+    val bikingSpeed = speed(time = 2.85)
+    val drivingSpeed = speed(time = 0.37)
     val flyingSpeed = speed(0.12, 0.01)
 
     println(walkingSpeed)
