606511d9e858574991a3dcb091abda19da334014
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 14:57:23 +0300

    Listing 2.15 - A function with a default second argument

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,8 @@ fun main() {
     val distance = 321.8
     val time = 4.15
 
-    fun speed(distance: Double, time: Double) = distance / time
+    fun speed(distance: Double, time: Double = 2.0) = distance / time
+
     val averageSpeed = speed(time = 4.15, distance = 321.8)
     val averageSpeed1 = speed(321.8, 4.15)
     val averageSpeed2 = speed(distance = 321.8, 4.15)
