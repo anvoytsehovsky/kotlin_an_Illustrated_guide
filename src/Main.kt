bd1e2088c842697662e779e3d35e149d088f96a1
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 15:14:26 +0300

    Listing 2.19 - Error: "No value passed for parameter ‘time" (does not compile)

--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,9 @@ fun main() {
 
     fun speed(distance: Double = 42.195, time: Double) = distance / time
 
-    val walkingSpeed = speed(42.195, 8.27)
-    val bikingSpeed = speed(42.195, 2.85)
-    val drivingSpeed = speed(42.195, 0.37)
+    val walkingSpeed = speed(8.27)
+    val bikingSpeed = speed(2.85)
+    val drivingSpeed = speed(0.37)
     val flyingSpeed = speed(0.12, 0.01)
 
     println(walkingSpeed)
