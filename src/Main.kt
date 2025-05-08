858137fa93358d9ec66165e712aaebc96bf1e363
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 15:00:49 +0300

    Listing 2.17 - Calling a function with the same argument value. The distance is 42.195 for the first three function calls

--- src/Main.kt
+++ src/Main.kt
@@ -4,17 +4,10 @@ fun main() {
 
     fun speed(distance: Double, time: Double = 2.0) = distance / time
 
-    val averageSpeed = speed(time = 4.15, distance = 321.8)
-    val averageSpeed1 = speed(321.8, 4.15)
-    val averageSpeed2 = speed(distance = 321.8, 4.15)
-    val averageSpeed3 = speed(321.8, time = 4.15)
-    val averageSpeed4 = speed(distance = 321.8, time = 4.15)
-    val averageSpeed5 = speed(time = 4.15, distance = 321.8)
-
-    val walkingSpeed = speed(10.2)
-    val bikingSpeed = speed(29.6)
-    val drivingSpeed = speed(225.3)
-    val flyingSpeed = speed(1368.747, 1.5)
+    val walkingSpeed = speed(42.195, 8.27)
+    val bikingSpeed = speed(42.195, 2.85)
+    val drivingSpeed = speed(42.195, 0.37)
+    val flyingSpeed = speed(0.12, 0.01)
 
 
 }
