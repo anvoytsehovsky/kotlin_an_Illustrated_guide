c4a30ee82682ca474aa146cc07b09a2e1de15d7e
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 08:35:15 +0300

    Listing 2.2 - Calculating the circumference of a circle that has a radius of 5.2, and another with radius 6.7, and another with radius 10.0

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 fun main() {
     val pi = 3.14
+
     var radius = 5.2
-    val circumference = 2 * pi * radius
+    val  circumferenceOfSmallCircle = 2 * pi * radius
+
+    radius = 6.7
+    val  circumferenceOfMediumCircle = 2 * pi * radius
+
+    radius = 10.0
+    val  circumferenceOfLargeCircle = 2 * pi * radius
 }
