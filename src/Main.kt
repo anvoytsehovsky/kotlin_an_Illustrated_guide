eede19cfe82b5caa0e9b7035d480194ad837d874
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 11:56:46 +0300

    Listing 2.12 - Using named arguments to reverse the order of two arguments

--- src/Main.kt
+++ src/Main.kt
@@ -3,5 +3,5 @@ fun main() {
     val time = 4.15
 
     fun speed(distance: Double, time: Double) = distance / time
-    val averageSpeed = speed(distance = 321.8, time = 4.15)
+    val averageSpeed = speed(time = 4.15, distance = 321.8)
 }
