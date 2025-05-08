2f7530f565d87ef14256f204c7b9f10687edc543
andrew-wojciechovsky <anvoytsehovsky@gmail.com>
2025-05-08 14:56:05 +0300

    Listing 2.14 - Calling a function with the same argument value. The time is 2.0 for the first three function calls.

--- src/Main.kt
+++ src/Main.kt
@@ -9,4 +9,10 @@ fun main() {
     val averageSpeed3 = speed(321.8, time = 4.15)
     val averageSpeed4 = speed(distance = 321.8, time = 4.15)
     val averageSpeed5 = speed(time = 4.15, distance = 321.8)
+
+    val walkingSpeed = speed(10.2, 2.0)
+    val bikingSpeed = speed(29.6, 2.0)
+    val drivingSpeed = speed(225.3, 2.0)
+    val flyingSpeed = speed(1368.747, 1.5)
+
 }
